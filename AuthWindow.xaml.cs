5857551864c374c3e1840cc21556b1f645afbe7c
Mishka-Dss <misha.gorelkin.gh@gmail.com>
2025-03-19 14:32:37 +0300

    Add files via upload

---
+++ AuthWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppDautov.Logic;
+using WpfAppKolodchenkoVitaly.Logic;
+
+namespace WpfAppKolodchenkoVitaly
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        private IAuthService _authService;
+
+        public AuthWindow(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public AuthWindow()
+        {
+            InitializeComponent();
+            _authService = new AuthService();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            string login = tbxlogin.Text;
+            string pass = tbxPass.Text;
+
+            if (_authService.CheckData(login, pass))
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка, проверьте правильность введённых данных в полях");
+            }
+        }
+    }
+}
---
+++ DataGrid.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppKolodchenkoVitaly
+{
+    /// <summary>
+    /// Логика взаимодействия для DataGrid.xaml
+    /// </summary>
+    public partial class DataGrid : Window
+    {
+        private object dbContext;
+
+        public DataGrid()
+        {
+            InitializeComponent();
+        }
+
+        public object DbContext { get => dbContext; set => dbContext = value; }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Products selectedProduct = ProductsDataGrid.SelectedItem as Products;
+            if (selectedProduct == null) 
+            {
+                MassageBox.Show("Выберите товар для редактирования");
+                return;
+            }
+            {
+                AddEditProductWindow editWindow = new AddEditProductWindow(selectedProduct, DbContext);
+                {
+                    LoadData();
+                }
+            }
+        }
+
+        private void LoadData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class AddEditProductWindow
+    {
+        private Products selectedProduct;
+        private object dbContext;
+
+        public AddEditProductWindow(Products selectedProduct, object dbContext)
+        {
+            this.SelectedProduct = selectedProduct;
+            this.DbContext = dbContext;
+        }
+
+        public object DbContext { get => dbContext; set => dbContext = value; }
+        internal Products SelectedProduct { get => selectedProduct; set => selectedProduct = value; }
+
+        internal bool ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class MassageBox
+    {
+        internal static void Show(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Products
+    {
+    }
+}
+
